b3aae00ac604351b9c55b59af3d35997e30863f8
Anton <anton.backstrom@gmail.com>
2019-07-29 21:34:23 +0200

    Setup the project
    Included everything needed for SDL2 to work

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(int argc, char* args[])
+{
+    printf("Hello world!\n");
+    return 0;
+}
