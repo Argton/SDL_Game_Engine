1f27ea619a22ce4b20f326dfeee242b3798697de
Anton <anton.backstrom@gmail.com>
2019-08-06 21:52:20 +0200

    Entered some variables and functions for rendering and displaying textures

--- main.c
+++ main.c
@@ -7,6 +7,415 @@
 #include <stdbool.h>
 #include <string.h>
 
+/************************************
+*
+*
+* Constants
+*
+*
+*************************************/
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+/************************************
+*
+*
+* Variables
+*
+*
+*************************************/
+
+int gTotalDisplays = 0;
+
+// Textures to render
+SDL_Texture* newTexture = NULL;
+SDL_Texture* mTexture;
+SDL_Texture* gTexture = NULL;
+
+SDL_Rect *gDisplayBounds = NULL;
+
+SDL_Color textColor = { 0, 0, 0, 0xFF };
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+/************************************
+*
+*
+* Structs
+*
+*
+*************************************/
+
+struct textureStruct
+{
+    char *imagePath;
+    int mWidth;
+    int mHeight;
+    int xPos;
+    int yPos;
+    SDL_Texture* mTexture;
+};
+
+struct LWindow
+{
+    SDL_Renderer* mRenderer;
+    int mWindowID;
+    int mWindowDisplayID;
+    SDL_Window* mWindow;
+    int mWidth;
+    int mHeight;
+    bool mMouseFocus;
+    bool mKeyboardFocus;
+    bool mFullScreen;
+    bool mMinimized;
+    bool mShown;
+};
+
+struct ttfStruct
+{
+    char *imagePath;
+    int mWidth;
+    int mHeight;
+    int xPos;
+    int yPos;
+    SDL_Texture* mTexture;
+    char *textureText;
+    SDL_Color textColor;
+};
+
+/************************************
+*
+*
+* Functions
+*
+*
+*************************************/
+
+bool init()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_AUDIO  ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Initialize PNG loading
+            int imgFlags = IMG_INIT_PNG;
+            if( !( IMG_Init( imgFlags ) & imgFlags ) )
+            {
+                printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+                success = false;
+            }
+            else
+            {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+            }
+        }
+    }
+        //Initialize SDL_mixer
+        if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
+        {
+            printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
+            success = false;
+        }
+    return success;
+}
+
+
+bool initLWindow(struct LWindow *inputStruct)
+{
+    inputStruct->mWindow = NULL;
+    inputStruct->mRenderer = NULL;
+    inputStruct->mWidth = SCREEN_WIDTH;
+    inputStruct->mHeight = SCREEN_HEIGHT;
+    inputStruct->mMouseFocus = false;
+    inputStruct->mKeyboardFocus = false;
+    inputStruct->mFullScreen = false;
+    inputStruct->mMinimized = false;
+    inputStruct->mWindowID = -1;
+    return inputStruct->mWindow == NULL;
+}
+
+bool initRenderer()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_AUDIO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Create renderer for window
+            gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED);
+            if( gRenderer == NULL )
+            {
+                printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+                success = false;
+            }
+            else
+            {
+                //Initialize renderer color
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+                //Initialize PNG loading
+                int imgFlags = IMG_INIT_PNG;
+                if( !( IMG_Init( imgFlags ) & imgFlags ) )
+                {
+                    printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+                    success = false;
+                }
+                else
+                {
+                    //Get window surface
+                    gScreenSurface = SDL_GetWindowSurface( gWindow );
+                }
+            }
+        }
+    }
+    //Initialize SDL_ttf
+    if( TTF_Init() == -1 )
+    {
+        printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() );
+        success = false;
+    }
+    //Initialize SDL_mixer
+    if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
+    {
+        printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
+        success = false;
+    }
+    return success;
+}
+
+bool initLWindowRenderer(struct LWindow *inputStruct)
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_AUDIO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        gTotalDisplays = SDL_GetNumVideoDisplays();
+        if( gTotalDisplays < 2 )
+        {
+            printf( "Warning: Only one display connected!" );
+        }
+
+        gDisplayBounds = malloc (gTotalDisplays * sizeof(SDL_Rect) );
+
+        for( int i = 0; i < gTotalDisplays; ++i )
+        {
+            SDL_GetDisplayBounds( i, &gDisplayBounds[ i ] );
+        }
+        inputStruct->mWindow = NULL;
+
+        //Create window
+        inputStruct->mWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( inputStruct->mWindow == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            inputStruct->mMouseFocus = true;
+            inputStruct->mKeyboardFocus = true;
+            inputStruct->mWidth = SCREEN_WIDTH;
+            inputStruct->mHeight = SCREEN_HEIGHT;
+            //Create renderer for window
+            inputStruct->mRenderer = SDL_CreateRenderer( inputStruct->mWindow, -1, SDL_RENDERER_ACCELERATED);
+            if( inputStruct->mRenderer  == NULL )
+            {
+                printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+                success = false;
+            }
+            else
+            {
+                //Initialize renderer color
+                SDL_SetRenderDrawColor( inputStruct->mRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+                //Initialize PNG loading
+                int imgFlags = IMG_INIT_PNG;
+                if( !( IMG_Init( imgFlags ) & imgFlags ) )
+                {
+                    printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+                    success = false;
+                }
+                else
+                {
+                    //Get window surface
+                    gScreenSurface = SDL_GetWindowSurface( inputStruct->mWindow );
+                    //Grab window identifier
+                    inputStruct->mWindowID = SDL_GetWindowID( inputStruct->mWindow );
+                    inputStruct->mWindowDisplayID = SDL_GetWindowDisplayIndex( inputStruct->mWindow );
+                    //Flag as opened
+                    inputStruct->mShown = true;
+                }
+            }
+        }
+    }
+    //Initialize SDL_ttf
+    if( TTF_Init() == -1 )
+    {
+        printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() );
+        success = false;
+    }
+    //Initialize SDL_mixer
+    if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
+    {
+        printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
+        success = false;
+    }
+    return inputStruct->mWindow != NULL && inputStruct->mRenderer != NULL && success;
+}
+
+
+SDL_Surface* loadSurface( char *path )
+{
+    //The final optimized image
+	SDL_Surface* optimizedSurface = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load( path );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL Error: %s\n", path, IMG_GetError() );
+    }
+    else
+    {
+       //Convert surface to screen format
+		optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, 0 );
+		if( optimizedSurface == NULL )
+		{
+			printf( "Unable to optimize image %s! SDL Error: %s\n", path, SDL_GetError() );
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	return optimizedSurface;
+}
+
+SDL_Texture* loadTexture( char *path )
+{
+    //The final texture
+    SDL_Texture* newTexture = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load( path );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError() );
+    }
+    else
+    {
+        //Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+        if( newTexture == NULL )
+        {
+            printf( "Unable to create texture from %s! SDL Error: %s\n", path, SDL_GetError() );
+        }
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+    return newTexture;
+}
+
+
+bool LTexture(struct textureStruct *structinput)
+{
+    mTexture = NULL;
+
+    SDL_Surface* loadedSurface = IMG_Load( structinput->imagePath );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", structinput->imagePath, IMG_GetError() );
+    }
+    else
+    {
+        //Color key image
+        SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
+        //Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+        if( newTexture == NULL )
+        {
+            printf( "Unable to create texture from %s! SDL Error: %s\n", structinput->imagePath, SDL_GetError() );
+        }
+        else
+        {
+            //Get image dimensions
+            structinput->mWidth = loadedSurface->w;
+            structinput->mHeight = loadedSurface->h;
+        }
+
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+    //Return success
+    structinput->mTexture = newTexture;
+    return newTexture != NULL;
+}
+
+
+void textureRender(struct textureStruct *structinput, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip, int posX, int posY)
+{
+    //Set rendering space and render to screen
+    SDL_Rect renderQuad = { structinput->xPos+ posX, structinput->yPos+ posY, structinput->mWidth, structinput->mHeight };
+
+    //Set clip rendering dimensions
+    if( clip != NULL )
+    {
+        renderQuad.w = clip->w;
+        renderQuad.h = clip->h;
+    }
+    //Render to screen
+    //SDL_RenderCopy( gRenderer, structinput->mTexture, clip, &renderQuad );
+    SDL_RenderCopyEx( gRenderer, structinput->mTexture, clip, &renderQuad, angle, center, flip );
+}
+
+
 int main(int argc, char* args[])
 {
     printf("Hello world!\n");
